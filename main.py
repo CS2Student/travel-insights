7569ccd7bbd940374e9bc1412a4b1943a1c5a0fc
aidenhu <99224270+CS2Student@users.noreply.github.com>
2023-08-17 16:46:12 -0400

    Parse necessary values

--- main.py
+++ main.py
@@ -18,3 +18,18 @@ def kelvin_to_celsius_fahrenheit(kelvin):
     celsius = kelvin - 273.15
     fahrenheit = celsius * (9/5) + 32
     return celsius, fahrenheit
+
+
+# Parse necessary values
+temp_kelvin = response['main']['temp']
+temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
+feels_like_kelvin = response['main']['feels_like']
+feels_like_celsius, feels_like_fahrenheit = kelvin_to_celsius_fahrenheit(
+    feels_like_kelvin)
+wind_speed = response['wind']['speed']
+humidity = response['main']['humidity']
+description = response['weather'][0]['description']
+sunrise_time = dt.datetime.utcfromtimestamp(
+    response['sys']['sunrise'] + response['timezone'])
+sunset_time = dt.datetime.utcfromtimestamp(
+    response['sys']['sunset'] + response['timezone'])
