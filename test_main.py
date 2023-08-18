cd54d6f43475e2220bd1d98b347f0bb1dc1d5a64
aidenhu <99224270+CS2Student@users.noreply.github.com>
2023-08-17 22:28:40 -0400

    Implement jetlag function edge case unit tests

--- test_main.py
+++ test_main.py
@@ -11,13 +11,33 @@ class TestCalc(unittest.TestCase):
 
     # Test Origin time ahead of Destination time
     def test_jetlag_rec_OAD(self):
-        result = main.jetlag_rec("22:30", "New York", "Hawaii")
-        self.assertEqual(result, "04:30")
+        result = main.jetlag_rec("10:00", "New York", "Hawaii")
+        self.assertEqual(result, "16:00")
 
     # Test Destination time ahead of Origin time
     def test_jetlag_rec_DAO(self):
         result = main.jetlag_rec("22:30", "New York", "Beijing")
         self.assertEqual(result, "10:30")
+
+    # Test Origin Time same as Destination Time
+    def test_jetlag_rec_equal(self):
+        result = main.jetlag_rec("00:30", "New York", "Toronto")
+        self.assertEqual(result, "00:30")
+
+     # Test time = 00:00
+    def test_jetlag_rec_0(self):
+        result = main.jetlag_rec("00:00", "New York", "Toronto")
+        self.assertEqual(result, "00:00")
+
+    # Test time = 24:00, works because 24:00 == 00:00 in 24 hr time
+    def test_jetlag_rec_24(self):
+        result = main.jetlag_rec("24:00", "New York", "Toronto")
+        self.assertEqual(result, "00:00")
+
+    # Tests time > 24
+    def test_jetlag_rec_over(self):
+        result = main.jetlag_rec("24:01", "New York", "Toronto")
+        self.assertEqual(result, False)
 
 
 if __name__ == '__main__':
